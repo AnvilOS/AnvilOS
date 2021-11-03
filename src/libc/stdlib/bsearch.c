679af77c05080023792454f1b4e518c1a9ccf615
Gerard Green <gerryg@anvilos.com>
2021-11-04 07:58:51 +1100

    Implemented a vanilla bsearch - needs tests

--- src/libc/stdlib/bsearch.c
+++ src/libc/stdlib/bsearch.c
@@ -3,5 +3,28 @@
 
 void *bsearch(const void *key, const void *base, size_t nmemb, size_t size, int (*compar)(const void *, const void *))
 {
+    size_t l = 0;
+    size_t r = nmemb - 1;
+
+    while (l <= r)
+    {
+        // Find the middle index without overflowing
+        size_t m = l + (r - l) / 2;
+        // and from that the middle element
+        void *mel = (char *)base + m * size;
+        int cmp = compar(mel, key);
+        if (cmp < 0 )
+        {
+            l = m + 1;
+        }
+        else if (cmp > 0)
+        {
+            r = m - 1;
+        }
+        else
+        {
+            return mel;
+        }
+    }
     return NULL;
 }
